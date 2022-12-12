5404723cdc2a3517bf671e616e509d2f653afa5a
Jonathan Larochelle <jonathan.larochelle.3@gmail.com>
2022-12-12 20:39:00 +0100

    2022 - Day 12, finally, with help from the internet

--- 2022/Day 12/solution.py
+++ 2022/Day 12/solution.py
@@ -2,15 +2,13 @@
 # Jonathan Larochelle
 
 import time
-import copy
-
+import os
+os.environ["TERM"] = "xterm"
 
 def get_input():
     with open((__file__.rstrip("solution.py")+"input.txt"), 'r') as input_file:
         input = input_file.read()
     return input
-
-nb_step_good_paths = []
 
 
 def part_one():
@@ -20,88 +18,135 @@ def part_one():
     nb_rows = len(input)
     nb_cols = len(input[0])
 
-
+    node_scores = []
     elevation = []
+    unvisited_nodes = []
 
+    # Convert letters to elevation values from 1 to 26
     for y in range(nb_rows):
-        row = []
+        ns_row = []
+        e_row = []
         for x in range(nb_cols):
             if input[y][x] == "S":
-                start_x = x
-                start_y = y
-                row.append(ord("a"))
+                e_row.append(1)
+                ns_row.append(0)
             elif input[y][x] == "E":
-                end_x = x
-                end_y = y
-                row.append(ord("z"))
+                end = (x, y)
+                e_row.append(26)
+                ns_row.append(-1)
             else:
-                row.append(ord(input[y][x]))
-        elevation.append(row)
+                e_row.append(ord(input[y][x]) - 96)
+                ns_row.append(-1)
+            unvisited_nodes.append((x, y))
+        node_scores.append(ns_row)
+        elevation.append(e_row)
 
-    minimum_nb_steps = None
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-    class Step:
-        def __init__(self, parent, x0, y0):
-            self.x0 = x0
-            self.y0 = y0
-            if parent:
-                self.step_nb = parent.step_nb + 1
-            else:
-                self.step_nb = 0
-            self.children = []
-            self.parent = parent
+    while len(unvisited_nodes) > 1 and node_scores[end[1]][end[0]] == -1:
+        # Focus on unvisited node with lower score
+        node = None
+        for un in unvisited_nodes:
+            if node_scores[un[1]][un[0]] != -1:
+                if not node:
+                    node = un
+                elif node_scores[un[1]][un[0]] < node_scores[node[1]][node[0]]:
+                    node = un
 
-            print(f"{self.x0}, {self.y0}, {elevation[self.y0][self.x0]}")
+        # Look at four directions
+        for d in directions:
+            next = (node[0] + d[0], node[1] + d[1])
+            if next in unvisited_nodes:
+                if elevation[next[1]][next[0]] - 1 <= elevation[node[1]][node[0]]:
+                    # We can access the next node
 
-            if self.step_nb < 200:
-                if (x0 == end_x) and (y0 == end_y):
-                    print(f"Success in {self.step_nb} steps!")
-                    nb_step_good_paths.append(self.step_nb)
-                else:
-                    self.move(x0 + 1, y0)
-                    self.move(x0, y0 - 1)
-                    self.move(x0, y0 + 1)
-                    self.move(x0 - 1, y0)
+                    if node_scores[next[1]][next[0]] == -1:
+                        node_scores[next[1]][next[0]] = node_scores[node[1]][node[0]] + 1
+                    elif node_scores[node[1]][node[0]] + 1 < node_scores[next[1]][next[0]]:
+                        node_scores[next[1]][next[0]] = node_scores[node[1]][node[0]] + 1
 
+        # Node is visited
+        unvisited_nodes.remove(node)
 
-        def move(self, x1, y1):
-            if elevation[y1][x1] != 97:
-                if not self.is_in_history(x1, y1):
-                    if 0 <= x1 < nb_cols and 0 <= y1 < nb_rows:
-                        if 0 <= elevation[y1][x1] - elevation[self.y0][self.x0] <= 1:
-                            self.children.append(Step(self, x1, y1))
-
-        def is_in_history(self, x, y):
-            if (x == self.x0) and (y == self.y0):
-                return True
-            else:
-                if self.parent:
-                    return self.parent.is_in_history(x, y)
-                else:
-                    return False
-
-    first_step = Step(None, start_x, start_y)
+        # Progress
+        print(len(unvisited_nodes))
 
     end_time = time.time()
     print(f"Part One")
     print(f"\t- Solved in {(end_time-start_time) * 10**3}ms.")
-    print(f"\t - Output: {min(nb_step_good_paths)}")
+    print(f"\t - Output: {node_scores[end[1]][end[0]]}")
 
 
 def part_two():
+    # This time, we start from the end, and walk until all values on the left line are stated.
     start_time = time.time()
-    input = get_input().split("\n\n")
+    input = get_input().splitlines()
 
+    nb_rows = len(input)
+    nb_cols = len(input[0])
+
+    node_scores = []
+    elevation = []
+    unvisited_nodes = []
+
+    # Convert letters to elevation values from 1 to 26
+    for y in range(nb_rows):
+        ns_row = []
+        e_row = []
+        for x in range(nb_cols):
+            if input[y][x] == "S":
+                e_row.append(1)
+                ns_row.append(-1)
+            elif input[y][x] == "E":
+                e_row.append(26)
+                ns_row.append(0)
+            else:
+                e_row.append(ord(input[y][x]) - 96)
+                ns_row.append(-1)
+            unvisited_nodes.append((x, y))
+        node_scores.append(ns_row)
+        elevation.append(e_row)
+
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    while len(unvisited_nodes) > 1 and min([node_scores[row][0] for row in range(nb_rows)]) == -1:
+        # Focus on unvisited node with lower score
+        node = None
+        for un in unvisited_nodes:
+            if node_scores[un[1]][un[0]] != -1:
+                if not node:
+                    node = un
+                elif node_scores[un[1]][un[0]] < node_scores[node[1]][node[0]]:
+                    node = un
+
+        # Look at four directions
+        for d in directions:
+            next = (node[0] + d[0], node[1] + d[1])
+            if next in unvisited_nodes:
+                # Current node can be reached from next if it is one more or lower
+                if elevation[next[1]][next[0]] + 1 >= elevation[node[1]][node[0]]:
+                    # We can access the next node
+
+                    if node_scores[next[1]][next[0]] == -1:
+                        node_scores[next[1]][next[0]] = node_scores[node[1]][node[0]] + 1
+                    elif node_scores[node[1]][node[0]] + 1 < node_scores[next[1]][next[0]]:
+                        node_scores[next[1]][next[0]] = node_scores[node[1]][node[0]] + 1
+
+        # Node is visited
+        unvisited_nodes.remove(node)
+
+        # Progress
+        print(len(unvisited_nodes))
 
     end_time = time.time()
     print(f"Part Two")
-    print(f"\t- Solved in {(end_time-start_time) * 10**3}ms.")
-    print(f"\t - Output: {monkey_business}")
+    print(f"\t- Solved in {(end_time - start_time) * 10 ** 3}ms.")
+    print(f"\t - Output: {min([node_scores[row][0] for row in range(nb_rows)])}")
 
-    #96577
+
 def main():
-    part_one()
-    #part_two()
+    # part_one()
+    part_two()
 
 
 if __name__ == '__main__':
